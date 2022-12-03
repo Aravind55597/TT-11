c5f77a6e5722b96295f58eef9fc9b122c46b9575
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-12-03 13:23:45 +0800

    create scheduled transactions

--- backend/routes/schedRoute.py
+++ backend/routes/schedRoute.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from controllers.schedController import get_scheduled_transactions_accountid
+from controllers.schedController import create_scheduled_transactions
 
 schedRoute = Blueprint('sched', __name__)
 
 schedRoute.route('/<int:AccountID>', strict_slashes=False, methods=['GET'])(get_scheduled_transactions_accountid)
+
+schedRoute.route('/', strict_slashes=False, methods=['POST'])(create_scheduled_transactions)
