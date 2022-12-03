191cd9e768c279552c3b73de2896a190560544aa
Wence <86223302+W3nce@users.noreply.github.com>
2022-12-03 13:21:05 +0800

    new updates

--- frontend/src/pages/TestHome.js
+++ frontend/src/pages/TestHome.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/authContext";
+import axios from "axios";
+import hosturl from "../hosturl";
+
 import ViewUserDetails from "./ViewUserDetails";
+import ViewPersonalDetails from "./ViewPersonalDetails";
 
 function TestHome() {
+  const user_ID = auth.info.UserID;
+  const token = auth.token;
+
+  let navigate = useNavigate();
+  const auth = useAuth();
   return (
-    <div>
-      <p>Personal Particular</p>
+    <div className="container-test">
       <ViewUserDetails></ViewUserDetails>
+
+      <ViewPersonalDetails></ViewPersonalDetails>
     </div>
   );
 }
