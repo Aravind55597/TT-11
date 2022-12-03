e523ff7b0664254e85d00fa9d799caa228dd622a
Aravind55597 <gowthamaravindfaiz@gmail.com>
2022-12-03 10:16:33 +0800

    Created Project Backbone

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import AuthContextProvider from "./contexts/authContext";
+function App() {
+
+    const [theme, setTheme] = useState(false);
+
+    const location = useLocation();
+
+    useEffect(() => {    
+        if (window.location.pathname === "/login"){
+        setTheme(true);
+      } else{
+        setTheme(false);
+      }
+    }, [location.pathname]);
+
+    return (
+        <AuthContextProvider>
+            <div className={`App ${theme ? 'background-image' : 'background-lightBlue'}`} style={{ minHeight: "100vh" }}>
+                <Header />                    
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </AuthContextProvider>
+    );
+}
+
+export default App;
